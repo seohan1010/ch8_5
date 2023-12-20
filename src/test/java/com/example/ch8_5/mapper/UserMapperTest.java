78084cc863c7a0ba0ead221489dcd41b5b74032f
seohan1010 <seohan1010@hotmail.com>
2023-12-20 23:46:22 +0900

    error occured - Incompatible because this component declares a component, compatible with Java 17 and the consumer needed a component, compatible with Java 11

--- src/test/java/com/example/ch8_5/mapper/UserMapperTest.java
+++ src/test/java/com/example/ch8_5/mapper/UserMapperTest.java
@@ -1,40 +1,32 @@
-package com.example.ch8_5.mapper;
-
-import com.example.ch8_5.to.UserDto;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-import javax.annotation.Resource;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-
-
-@SpringBootTest
-class UserMapperTest {
-
-
-    @Autowired
-    UserMapper userMapper;
-
-
-
-    @Test
-    public void test(){
-
-        System.out.println("<<<<<<<<< userMapper = " + userMapper);
-    }
-
-
-    @Test
-    public void selectUserTest()throws Exception{
-
-        String email ="aaa@aaa.com";
-
-       UserDto user= userMapper.selectUser(email);
-        assertNotNull(user);
-        System.out.println("<<<<<<<<< user = " + user);
-    }
-
-}
+//package com.example.ch8_5.mapper;
+//
+//import com.example.ch8_5.to.UserDto;
+//import org.junit.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//
+//
+//@SpringBootTest
+//class UserMapperTest {
+//
+//    @Autowired
+//    UserMapper userMapper;
+//
+//
+//    @Test
+//    public void test() {
+//
+//        System.out.println("<<<<<<<<< userMapper = " + userMapper);
+//    }
+//
+//
+//    @Test
+//    public void selectUserTest() throws Exception {
+//
+//        String email = "aaa@aaa.com";
+//
+//        UserDto user = userMapper.selectUser(email);
+//        assertNotNull(user);
+//        System.out.println("<<<<<<<<< user = " + user);
+//    }
+//
+//}
