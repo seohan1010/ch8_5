55cf5fe60d2323e604213d26eeb1c40165e73615
seohan1010 <seohan1010@hotmail.com>
2023-12-31 00:31:21 +0900

    BoardController.java added

---
+++ src/main/java/com/example/ch8_5/controller/BoardController.java
@@ -0,0 +1,12 @@
+package com.example.ch8_5.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/board")
+public class BoardController {
+
+
+
+}
