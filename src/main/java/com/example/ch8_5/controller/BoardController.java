0d8ab47b6652f7c70f7dfdbdc8c9ee14c2f8d114
seohan1010 <seohan1010@hotmail.com>
2024-01-01 21:58:15 +0900

    BoardController.java retrieveBoardList() added

--- src/main/java/com/example/ch8_5/controller/BoardController.java
+++ src/main/java/com/example/ch8_5/controller/BoardController.java
@@ -1,12 +1,28 @@
 package com.example.ch8_5.controller;
 
+import com.example.ch8_5.to.BoardDto;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @RestController
 @RequestMapping(value="/board")
 public class BoardController {
 
+    @RequestMapping(value="/board",method= RequestMethod.GET)
+    public List<BoardDto> retrieveBoardList(){
+        List<BoardDto> list = new ArrayList<>();
+        list.add(new BoardDto());
+        list.add(new BoardDto());
+        list.add(new BoardDto());
+
+        return list;
+
+    }
 
 
 }
