2c98bafd035ed994e3bafd9de956eacc83964974
seohan1010 <seohan1010@hotmail.com>
2024-01-05 23:47:11 +0900

    controller added to AuthenticationController.java

--- src/main/java/com/example/ch8_5/controller/AuthenticationController.java
+++ src/main/java/com/example/ch8_5/controller/AuthenticationController.java
@@ -1,10 +1,20 @@
 package com.example.ch8_5.controller;
 
 
+import com.example.ch8_5.to.UserDto;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins = {"http://localhost:3000", "*"})
 @RestController
+@RequestMapping(value="/auth")
 public class AuthenticationController {
+
+@RequestMapping(value="/login")
+    public void login(@RequestBody UserDto userDto){
+
+}
+
 }
