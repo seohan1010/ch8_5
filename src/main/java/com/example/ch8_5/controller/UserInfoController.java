c8886ae2b7a301cc5f42cc505bc991dbc9fdcfc0
seohan1010 <seohan1010@hotmail.com>
2024-01-08 21:40:56 +0900

    modifyUser controller added

--- src/main/java/com/example/ch8_5/controller/UserInfoController.java
+++ src/main/java/com/example/ch8_5/controller/UserInfoController.java
@@ -2,6 +2,8 @@ package com.example.ch8_5.controller;
 
 
 import com.example.ch8_5.to.UserDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,12 +14,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserInfoController {
 
 
-
+    // 유저를 등록하는 로직
     @RequestMapping(value="/user",method= RequestMethod.POST)
     public void registerUser(@RequestBody UserDto userDto){
 
 
 
+    }
+
+    // 유저의 정보를 수정하는 로직
+    @RequestMapping(value="/user",method=RequestMethod.PUT)
+    public ResponseEntity<String> modifyUser(@RequestBody UserDto userDto){
+        System.out.println("userDto = " + userDto);
+        return new ResponseEntity<>("good", HttpStatus.OK);
     }
 
 
