9d7445a6476ea5a02058d07caa4fc6aad98df23c
seohan1010 <seohan1010@hotmail.com>
2024-01-16 22:34:05 +0900

    File Crud  upload, delete started

--- src/main/java/com/example/ch8_5/common/WebConfig.java
+++ src/main/java/com/example/ch8_5/common/WebConfig.java
@@ -10,11 +10,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    @Value("@{spring.servlet.multipart.location}")
-    String filepath;
+    @Value("${spring.servlet.multipart.location}")
+    private String filepath;
 
     @Override
-    public void addCorsMappings(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("*");
 
@@ -22,9 +22,9 @@ public class WebConfig implements WebMvcConfigurer {
 
 
     @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry){
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
         // addResourceLocations 등록 시 어떤 방식으로 접근하는지 (file or classpath) 설정
-        registry.addResourceHandler("/uploads/**").addResourceLocations("file:"+filepath);
+        registry.addResourceHandler("/uploads/**").addResourceLocations("file:" + filepath);
     }
 
 
