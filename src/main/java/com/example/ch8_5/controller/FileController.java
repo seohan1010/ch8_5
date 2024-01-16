e27d75c16a88fd5e9154459cb477f1dda8813586
seohan1010 <seohan1010@hotmail.com>
2024-01-16 20:41:57 +0900

    .gitignore test

--- src/main/java/com/example/ch8_5/controller/FileController.java
+++ src/main/java/com/example/ch8_5/controller/FileController.java
@@ -1,7 +1,13 @@
 package com.example.ch8_5.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FileController {
+
+    @Value("@{spring.servlet.multipart.location}")
+    String filepath;
+
+
 }
