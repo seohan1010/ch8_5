7f96c7b91c659dcaf464807983c55c585e31ced2
seohan1010 <seohan1010@hotmail.com>
2025-10-08 15:48:07 +0900

    Update ChatUserController.java

--- src/main/java/com/example/ch8_5/controller/ChatUserController.java
+++ src/main/java/com/example/ch8_5/controller/ChatUserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
-@CrossOrigin(origins = {"http://localhost:3000", "*"})
+@CrossOrigin(origins = {"http://localhost:3001", "*"})
 @RestController
 @RequestMapping(value = "/chatuser")
 public class ChatUserController {
